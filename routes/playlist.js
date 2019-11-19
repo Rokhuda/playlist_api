2a68d62a27a315a0fa133b0be0e124f82f32ec48
rokhuda <rokhuda@gmail.com>
2019-11-19 22:47:18 +0200

    edit button functional

--- routes/playlist.js
+++ routes/playlist.js
@@ -4,14 +4,26 @@ const Musician = require('../models/musician.js');
 const MongoClient = require("mongodb");
 const url = 'mongodb://127.0.0.1:27017/playlistdb'
 
-//Adding a  new musician
+
+// List all musicians
+router.get('/all', async(req, res) => {
+    try {
+        const musician = await Musician.find();
+        res.json(musician)
+        res.redirect("/")
+    } catch (error) {
+
+    }
+})
+
+
+// Adding a  new musician
 router.post('/add', async(req, res) => {
     const musician = new Musician({
         name: req.body.name
     })
     try {
         const newMusician = await musician.save()
-            //res.status(201).json(newMusician)
         res.redirect('/')
     } catch (error) {
         res.status(400).json({ message: error.message })
@@ -20,14 +32,14 @@ router.post('/add', async(req, res) => {
 });
 
 // Deleting an existing musician 
-router.post('/delete', (req, res) => {
+router.post('/delete', async(req, res) => {
     const id = req.body;
     const objectId = require("mongodb").ObjectID
     MongoClient.connect(url, {
         useNewUrlParser: true
     }, (err, db) => {
         if (err) throw err;
-        var dbo = db.db("playlistdb");
+        const dbo = db.db("playlistdb");
         dbo.collection("musicians").deleteOne({
             "_id": objectId(id.id)
         }, (err, result) => {
@@ -38,62 +50,25 @@ router.post('/delete', (req, res) => {
     });
 })
 
-// // Editing an existing musician
-// router.patch('/edit', getMusician, async(req, res) => {
-//     if (req.body.name != null) {
-//         res.musician.name = req.body.name
+// Editing an existing musician
+router.post('/edit', async(req, res) => {
+    const id = req.body;
+    const objectId = require("mongodb").ObjectID
 
-//     }
-//     try {
-//         const updatedMusician = await res.musician.save()
-//        //
-//         res.status(201).json(updatedMusician)
-//         res.redirect('/')
-//     } catch (err){
-//         res.status(400).json({ message: err.message })
-//     }
-// })
-
-
-
-
-// // List all musicians in the playlist
-// router.get('/all', async(req, res) => {
-//     try {
-//         const musician = await Musician.find();
-//         res.json(musician)
-//         res.redirect('/')
-//     } catch (error) {    
-//         res.status(500).json({ message: err.message })
-//     }
-
-// })
-
-// // Sorting musicains list alphabetically
-// router.get('/sort', async(req, res) => {
-//     try {
-//         const musician = await newMusician.find().sort('name')
-//         res.json(musician)
-//         res.redirect('/')
-//     } catch (error) {
-//         res.status(500).json({ message: err.message })
-//     }
-
-// })
-
-// // Middleware function for gettig musician object by ID
-// async function getMusician(req, res, next) {
-//     try {
-//         musician = await Musician.find(req.body.id)
-//         if (musician == null) {
-//             return res.status(404).json({ message: 'Cant find musician' })
-//         }
-//     } catch (err) {
-//         return res.status(500).json({ message: err.message })
-//     }
-
-//     res.musician = musician
-//     next()
-// }
+    MongoClient.connect(url, {
+        useNewUrlParser: true
+    }, (err, db) => {
+        if (err) throw err;
+        const dbo = db.db("playlistdb");
+        dbo.collection('musicians').findAndModify({ "_id": objectId(id.id) }, [
+                ['_id', 'asc']
+            ], { $set: { name: req.body.name } },
+            (err, object) => {
+                if (err) throw err;
+                db.close();
+                res.redirect('/')
+            })
+    })
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
-const favicon = require('serve-favicon')
-const Musician = require('./models/musician.js')
 
 const cors = require('cors')
 app.use(cors())
@@ -25,18 +23,6 @@ app.use(express.json())
 const playlistRouter = require("./routes/playlist")
 app.use('/playlist', playlistRouter)
 
-// List all musicians
-app.get('/all', async(req, res) => {
-    try {
-        const musician = await Musician.find();
-        res.json(musician)
-        res.send(json(musician))
-        res.redirect("/")
-    } catch (error) {
-        console.log('fail')
-
-    }
-})
 
 const port = 3090
 app.listen(port, () => console.log(`server connected ${port}`))
